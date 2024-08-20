0d7f4cc0cf6411067b113c26ce6730b27ac55dc1
Gabriel <Gabriel.cod0411@gmail.com>
2024-08-20 15:56:23 -0300

    Primeira página

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+            <contador />
+        </div>
+    )
+}
+
+function contador() {
+    const [Contador, setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(Contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{Contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+export default Home
